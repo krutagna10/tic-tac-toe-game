bd7bae0d61d0120959d9c903a5133cc415ad347b
Krutagna <kpatel.kp7016@gmail.com>
2023-02-09 18:11:15 +0530

    initial commit

--- src/components/GameChoice/GameChoiceForm.js
+++ src/components/GameChoice/GameChoiceForm.js
@@ -6,40 +6,44 @@ const GameChoiceForm = () => {
     const [computerChoice, setComputerChoice] = useState('x');
 
     const clickHandler = (event) => {
-        console.log(event.target.checked)
+        setUserChoice(event.target.value);
+    }
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
     }
 
     return (
         <div className='choice__form-wrapper'>
-            <form className='choice__form flow'>
+            <form className='choice__form flow' onSubmit={formSubmitHandler}>
                 <div className='choice__form-control background-semi-dark-navy flow'>
                     <h1 className='choice__form-heading font-size-300 text-silver'>
                         Pick player 1's mark
                     </h1>
                     <div className='choice__label-wrapper flex'>
-                        <label
-                            className='choice__label choice__label--x'
-                            htmlFor='choice__input--x'
-                        />
-                        <label
-                            className='choice__label choice__label--o choice__label--active'
-                            htmlFor='choice__input--o'
-                        />
-                    </div>
-                    <div className='choice__input-wrapper'>
-                        <input type='radio'
-                               name='choice__input'
-                               id='choice__input--o'
-                               className='choice__input'
-                               onChange={clickHandler}
-                               value='o'
-                        />
                         <input type='radio'
                                name='choice__input'
                                id='choice__input--x'
                                className='choice__input'
                                onChange={clickHandler}
                                value='x'
+                        />
+                        <label
+                            className='choice__label choice__label--x'
+                            htmlFor='choice__input--x'
+                        />
+
+                        <input type='radio'
+                               name='choice__input'
+                               id='choice__input--o'
+                               className='choice__input'
+                               onChange={clickHandler}
+                               value='o'
+                               checked
+                        />
+                        <label
+                            className='choice__label choice__label--o'
+                            htmlFor='choice__input--o'
                         />
                     </div>
                     <p className='choice__form-text font-size-100'>Remember: X goes first</p>
