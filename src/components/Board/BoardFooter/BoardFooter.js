58f4b368b9c4a872b3c9ba16eb2e45d531a442e6
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-02-21 22:38:11 +0530

    optimizing the code

--- src/components/Board/BoardFooter/BoardFooter.js
+++ src/components/Board/BoardFooter/BoardFooter.js
@@ -1,29 +1,11 @@
 import Flex from '../../../layouts/Flex/Flex';
 import './BoardFooter.css'
 
-const BoardFooter = (props) => {
-    let xLabelText = '';
-    let oLabelText = '';
-    let xScore;
-    let oScore;
-
-    if (props.user.choice === 'x') {
-        // Setting the scores
-        xScore = props.scores.user;
-        oScore = props.scores.computer;
-
-        // Setting the label texts
-        xLabelText = 'X(YOU)';
-        oLabelText = 'O(CPU)';
-    } else {
-        // Setting the scores
-        xScore = props.scores.computer;
-        oScore = props.scores.user;
-
-        // Setting the label texts
-        xLabelText = 'X(CPU)';
-        oLabelText = 'O(YOU)';
-    }
+const BoardFooter = ({scores, user}) => {
+    const xLabelText = user.choice === 'x' ? 'X(You)' : 'X(Cpu)';
+    let oLabelText = user.choice === 'x' ? 'O(Cpu)' : 'O(You)'
+    let xScore = user.choice === 'x' ? scores.user : scores.computer;
+    let oScore = user.choice === 'x' ? scores.computer : scores.user;
 
     return (
         <Flex className='game__board-footer flex--justify-space flex--gap'>
@@ -33,7 +15,7 @@ const BoardFooter = (props) => {
             </div>
             <div className='game__score-wrapper background-silver'>
                 <p className='game__score-label'>Ties</p>
-                <p className='game__score game__score--draw'>{props.scores.draw}</p>
+                <p className='game__score game__score--draw'>{scores.draw}</p>
             </div>
             <div className='game__score-wrapper background-yellow'>
                 <p className='game__score-label'>{oLabelText}</p>
