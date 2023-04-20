4c9449d1ef77da8b1b23224fc436d4acf22de374
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-04-20 12:25:27 +0530

    starting with the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
-import Choice from "./components/Choice/Choice";
-import Game from "./components/Game/Game";
 import GameProvider from "./context/GameProvider";
-import { useState } from "react";
-import Result from "./components/Result/Result";
+import Choice from "./components/Choice/Choice";
 
 function App() {
-  const [isChoiceSelected, setIsChoiceSelected] = useState(false);
-  const [isGameFinished, setIsGameFinished] = useState(false);
-
-  function handleStartGame() {
-    setIsChoiceSelected(true);
-  }
-
-  function handleGameFinished() {
-    setIsGameFinished(true);
-  }
-
-  function handlePlayAgain() {
-    setIsGameFinished(false);
-  }
-
-  function handleQuit() {
-    setIsChoiceSelected(false);
-    setIsGameFinished(false);
-  }
-
   return (
     <div className="app">
       <GameProvider>
         <h1>Tic Tac Toe Game</h1>
-        {!isChoiceSelected && <Choice onStartGame={handleStartGame} />}
-        {isChoiceSelected && !isGameFinished && (
-          <Game onGameFinished={handleGameFinished} />
-        )}
-        {isGameFinished && (
-          <Result onPlayAgain={handlePlayAgain} onQuit={handleQuit} />
-        )}
+        <Choice />
       </GameProvider>
     </div>
   );
